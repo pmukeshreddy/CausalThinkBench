fbf6c0de9d30edf9e978bb48021dc724322f55bb
mukesh reddy p <88029886+pmukeshreddy@users.noreply.github.com>
2025-07-02 15:37:50 -0400

    Update fine_tunning.py

--- phase1/fine_tunning.py
+++ phase1/fine_tunning.py
@@ -4,6 +4,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from peft import LoraConfig, PeftModel
 from trl import SFTTrainer
 import os
+from trl import SFTConfig
+from transformers import DataCollatorForLanguageModeling
+
+
 
 
 from huggingface_hub import login
@@ -12,7 +16,7 @@ from google.colab import userdata
 hf_token = userdata.get("HUGGINGFACE_TOKEN")
 login(hf_token)
 
-dataset_path = ""
+dataset_path = "/content/causal_think_bench_dataset_corrected.jsonl"
 dataset = load_dataset("json",data_files=dataset_path,split="train")
 
 def format_prompt(example):
@@ -34,10 +38,11 @@ dataset = dataset.map(format_prompt)
 
 base_model = "mistralai/Mistral-7B-Instruct-v0.2"
 
-#configure quantization 
+#configure quantization
 bnb_congig = BitsAndBytesConfig(load_in_4bit=True,bnb_4bit_quant_type="nf4",bnb_4bit_compute_dtype=torch.bfloat16)
 
-model = AutoModelForCausalLM.from_pretrained(base_model,quantization_config=bnb_congig,device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(base_model,quantization_config=bnb_congig,device_map="auto",attn_implementation="eager"
+)
 
 tokenizer = AutoTokenizer.from_pretrained(base_model)
 
@@ -49,13 +54,32 @@ lora_config = LoraConfig(r=16,lora_alpha=32,target_modules=["q_proj", "k_proj", 
 
 new_model_name = "mistral-7b-causal-think-bench"
 
-training_args = TrainingArguments(output_dir=new_model_name,per_device_train_batch_size=4,gradient_accumulation_steps=4,
-                                  learning_rate=2e-4,num_train_epochs=3, 
-    logging_steps=10, 
-    save_steps=50, 
-    fp16=True, 
-     )
 
-trainer = SFTTrainer(model=model,train_dataset=dataset,peft_config=lora_config,dataset_text_field="text",max_seq_length=1024,tokenizer=tokenizer,args=training_args)
 
+sft_config = SFTConfig(
+    output_dir=new_model_name,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=4,
+    learning_rate=2e-4,
+    num_train_epochs=3,
+    logging_steps=10,
+    save_steps=50,
+    bf16=True,
+    max_seq_length=1024,  # This goes in SFTConfig now
+    packing=False,
+)
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer,
+    mlm=False,  # For causal LM, not masked LM
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=dataset,
+    peft_config=lora_config,
+    args=sft_config,
+    data_collator=data_collator,
+    formatting_func=lambda x: x["text"],
+
+)
 trainer.train()
